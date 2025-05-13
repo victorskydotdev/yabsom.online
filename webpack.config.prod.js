4b9bbd4428a46223adad9a02905134f0ce3e7f46
techyVS <victorsky90@gmail.com>
2025-05-13 22:04:30 +0100

    added a MiniCssExtractPlugin to be able to extract separate css files

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -3,6 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const common = require('./webpack.config.common.js');
 const { merge } = require('webpack-merge');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = merge(common, {
 	mode: 'production',
@@ -12,7 +13,22 @@ module.exports = merge(common, {
 		path: path.resolve(__dirname, 'dist'),
 	},
 
-	plugins: [new CleanWebpackPlugin()],
+	plugins: [new CleanWebpackPlugin(), new MiniCssExtractPlugin()],
+
+	module: {
+		rules: [
+			{
+				test: /\.s[ac]ss$/i,
+				use: [
+					MiniCssExtractPlugin.loader, // adding this here for production purpases to extract css into files
+
+					'css-loader', // Translates CSS into CommonJS
+
+					'sass-loader', // Compiles Sass to CSS
+				],
+			},
+		],
+	},
 
 	// optimization: {
 	// 	runtimeChunk: 'single',
